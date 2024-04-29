f491000b478113e320ab8a46dbee2f8049a43ef1
Adaddy1 <adarshnema6@gmail.com>
2024-04-29 16:03:26 +0530

    compareTo

---
+++ compareTo.java
@@ -0,0 +1,12 @@
+public class compareTo {
+    public static void main(String[] args) {
+        String s1 = "15";
+        String s2 = "15";
+        System.out.println(s1.compareTo(s2));
+    }
+    
+}
+
+//if s2 is bigger you get a negative value
+//if both same then you get 0
+//if s1 bigger then you get a positive value
