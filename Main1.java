2184996921062da29741473bc94a5e3283e4f3a7
Adaddy1 <adarshnema6@gmail.com>
2024-04-30 20:42:20 +0530

    Main1

---
+++ Main1.java
@@ -0,0 +1,11 @@
+public class Main1
+{
+	public static void main(String[] args)
+	{
+	    System.out.println("Welcome to JAP");
+	   	char ch1 = 'x';
+		System.out.println(ch1);
+		ch1++;
+		System.out.println(ch1);
+	}
+}
