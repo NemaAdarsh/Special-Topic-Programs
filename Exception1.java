77a405b2b7cdb4210b1d49ec419dbb2fac94b936
Adaddy1 <adarshnema6@gmail.com>
2024-05-27 15:47:06 +0530

    Divide By 0 Exception

---
+++ Exception1.java
@@ -0,0 +1,9 @@
+public class Exception1
+{
+	public static void main(String[] args)
+	{
+		int a = 5, b = 0;
+		int res = a / b;
+		System.out.println("Integer Division " + a + "/" + b + "=" + res);
+	}
+}
